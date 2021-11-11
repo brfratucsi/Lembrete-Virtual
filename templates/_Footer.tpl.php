0698d7918be2e5e137638b85ba8558ef83f0bf04
Silvio Fratuchelli <silvio.fratuchelli@fatec.sp.gov.br>
2021-11-10 21:06:54 -0300

    Teste de icones na navbar usando o bootstrap

--- templates/_Footer.tpl.php
+++ templates/_Footer.tpl.php
@@ -2,8 +2,12 @@
 	<div class="container">
 		<hr>
 		<footer>
-			<p class="muted"><small>&copy; <?php echo date('Y'); ?> DESENVOLVIMENTOSERVIDORES2FINAL</small></p>
+			<p class="muted"><small>&copy; <?php echo date('Y'); ?>Desenvolvimento para Servidores II - Silvio Fratuchelli</small></p>
 		</footer>
 	</div>
 	</body>
-</html>
+
+	</html>
+
+
+	<!-- DESENVOLVIMENTOSERVIDORES2FINAL -->
